bedb9d9ff2abbdc657936bc269cf28bd048c822b
Rohit Bisht -X (robisht - PERSISTENT SYSTEMS INC at Cisco) <robisht@cisco.com>
2023-06-18 20:50:39 +0530

    upto counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,42 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: []
+    };
+    // constructor(){
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+    renderTags(){
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
     }
+    handleIncrement = product => {
+        console.log(product)
+        this.setState({count: this.state.count + 1});
+    };
+    // doHandleincrement = () => {
+    //     this.handleIncrement({id: 1});
+    // }
     render() { 
         return (
         <div>
+            {this.state.tags.length === 0 && "Please create a new tag!"}
+            {this.renderTags()}
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
+            <button onClick={() => this.handleIncrement({product:2})} className='btn btn-secondary btn-sm'>Increment</button>
             </div>
             );
     }
-    getBadgeClasses() {
+    getBadgeClasses(){
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes 
     }
-
-    formatCount() {
-        const {count} = this.state;         //destructuring
-        return count === 0 ? 'Zero' : count;
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
  
